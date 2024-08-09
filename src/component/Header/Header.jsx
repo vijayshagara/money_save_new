770bcd320c3b14468bfc23a3e4e8b6c4ca27d858
vijayshagara <108860027+vijayshagara@users.noreply.github.com>
2024-08-09 22:29:45 +0530

    Update Header.jsx

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -175,6 +175,7 @@ export default function Header() {
               noWrap
               component="div"
               sx={{ display: { xs: "none", sm: "block" } }}
+              style={{color:"pink"}}
             >
               FITPEO
             </Typography>
